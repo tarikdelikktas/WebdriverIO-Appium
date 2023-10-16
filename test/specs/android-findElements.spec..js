753960018d720779c11f89918e03cc669f020e79
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-16 16:52:55 -0700

    finding elements by xpath

--- test/specs/android-findElements.spec..js
+++ test/specs/android-findElements.spec..js
@@ -1,5 +1,3 @@
-const exp = require("constants")
-
 describe('Android Elements Tests', () => {
     it('Find Element by accessibiliy id', async () => {
         // find element by accesibility id
@@ -20,5 +18,20 @@ describe('Android Elements Tests', () => {
         
         // assertion
         await expect(apiDemos).toHaveText('API Demos')
+    })
+
+    it("Find element by XPath", async () => {
+        // xpath - (//tagname[@attribute=value])
+        await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click()
+
+        // find by resource-id
+        await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click()
+
+        // find element by text
+        await $('//android.widget.TextView[@text="Command two"]').click()
+
+        // find by class and assert by text
+        const textAssertion = await $('//android.widget.TextView')
+        await expect(textAssertion).toHaveText('You selected: 1 , Command two')
     })
 })
