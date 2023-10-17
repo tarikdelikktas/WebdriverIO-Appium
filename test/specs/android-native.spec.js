0c55a6b9bc45afd806783e25519c2ad11e69cf30
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-17 11:24:30 -0700

    dialog box for accept alert

--- test/specs/android-native.spec.js
+++ test/specs/android-native.spec.js
@@ -3,4 +3,10 @@ describe("Android Native Feature Tests",  () => {
         await driver.startActivity("io.appium.android.apis", "io.appium.android.apis.app.AlertDialogSamples")
         await expect($('//*[@text="App/Alert Dialogs"]')).toExist()
     })
+
+    it("Working with Dialog Boxes", async () => {
+        await $('//*[@resource-id="io.appium.android.apis:id/two_buttons"]').click()
+        await driver.acceptAlert() 
+        await expect($('//*[@resource-id="android:id/alertTitle"]')).not.toExist()  // assertion - alert box is no longer visible
+    })
 })
