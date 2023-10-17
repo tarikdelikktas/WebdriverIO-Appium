90938b0b7414aba3231bbbe294d69f416538961b
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-17 15:39:38 -0700

    refactored add/edit note screen files with POM

--- wdio.conf.js
+++ wdio.conf.js
@@ -24,7 +24,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-        './test/specs/android/delete*.js'
+        './test/specs/android/delete-note*.js'
     ],
     // Patterns to exclude.
     exclude: [
