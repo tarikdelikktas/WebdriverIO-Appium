b6f6ab907838c8a89b6165ce44f306c2d2e94b59
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-22 18:23:42 -0700

    scrollable element for pickers

--- test/specs/ios-native.spec.js
+++ test/specs/ios-native.spec.js
@@ -14,4 +14,22 @@ describe('iOS Native Features', () => {
         // assertion
         await expect($('~OK')).not.toExist()
     })
+
+    it.only('Working with Scrollable elements', async () => {
+        // easiest
+        // await driver.execute('mobile: scroll', {direction: "down"})
+        // await driver.execute('mobile: scroll', {direction: "up"})
+
+        // complex scenario for pickers
+        await $('~Picker View').click()
+
+        const redPicker = await $('~Red color component value')
+        const bluePicker = await $('~Blue color component value')
+
+        await driver.execute('mobile: scroll', {element: redPicker.elementId, direction: "down"})
+        await driver.execute('mobile: scroll', {element: bluePicker.elementId, direction: "up"})
+
+
+        await driver.pause(3000)
+    })
 })
