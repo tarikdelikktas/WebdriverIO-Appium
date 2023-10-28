c0bfc357bae42ca962613546574af46843474ccd
Tarik Deliktas <tarikdeliktas@gmail.com>
2023-10-27 19:22:28 -0700

    auto generate allure report by onComplete

--- config/wdio.shared.conf.js
+++ config/wdio.shared.conf.js
@@ -1,3 +1,5 @@
+const allure = require('allure-commandline')
+
 exports.config = {
 
     // Patterns to exclude.
@@ -102,8 +104,7 @@ exports.config = {
         if (error) {
             await browser.takeScreenshot(); 
         }
-    }
-
+    },
 
     /**
      * Hook that gets executed after the suite has ended
@@ -145,8 +146,26 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+    onComplete: function(exitCode, config, capabilities, results) {
+        const reportError = new Error('Could not generate Allure report')
+        const generation = allure(['generate', 'allure-results', '--clean'])
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(
+                () => reject(reportError),
+                5000)
+
+            generation.on('exit', function(exitCode) {
+                clearTimeout(generationTimeout)
+
+                if (exitCode !== 0) {
+                    return reject(reportError)
+                }
+
+                console.log('Allure report successfully generated')
+                resolve()
+            })
+        })
+    },
     /**
     * Gets executed when a refresh happens.
     * @param {String} oldSessionId session ID of the old session
